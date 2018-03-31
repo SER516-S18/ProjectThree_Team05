1496474790253bbe63d2515a997b022bd4013688
Aanchal Mahajan <aanchal.mahajan93@gmail.com>
2018-03-31 15:20:14 -0700

    Size and other dimension changes in UI

--- project3.team5/src/main/java/project3/client/view/ClientUI.java
+++ project3.team5/src/main/java/project3/client/view/ClientUI.java
@@ -1,7 +1,6 @@
 package project3.client.view;
 
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,15 +11,18 @@ import javax.swing.JTabbedPane;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-import java.awt.Canvas;
-import javax.swing.JSplitPane;
 import javax.swing.JComboBox;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.JScrollBar;
 
 public class ClientUI extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
 
@@ -45,20 +47,19 @@ public class ClientUI extends JFrame {
 	 */
 	public ClientUI() {
 		setTitle("Client");
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 770, 494);
+		setBounds(100, 100, 1006, 608);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton Connect = new JButton("Connect");
-		Connect.setBounds(647, 6, 117, 29);
+		Connect.setBounds(863, 5, 117, 29);
 		contentPane.add(Connect);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(6, 43, 758, 430);
+		tabbedPane.setBounds(6, 41, 984, 528);
 		contentPane.add(tabbedPane);
 		
 		JPanel panel = new JPanel();
@@ -67,88 +68,109 @@ public class ClientUI extends JFrame {
 		panel.setLayout(null);
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(6, 11, 310, 478);
 		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_2.setBounds(6, 6, 206, 372);
 		panel.add(panel_2);
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(326, 11, 341, 478);
 		panel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_4.setBounds(224, 6, 206, 372);
 		panel.add(panel_4);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_5.setBounds(439, 6, 292, 372);
-		panel.add(panel_5);
-		
-		JLabel lblNewLabel = new JLabel("Blink            ");
-		panel_5.add(lblNewLabel);
-		
-		JSlider slider_1 = new JSlider();
-		panel_5.add(slider_1);
-		
-		JLabel lblNewLabel_1 = new JLabel("Right Wink   ");
-		panel_5.add(lblNewLabel_1);
-		
-		JSlider slider_2 = new JSlider();
-		panel_5.add(slider_2);
-		
-		JLabel lblNewLabel_2 = new JLabel("Left Wink     ");
-		panel_5.add(lblNewLabel_2);
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_3.setBounds(677, 11, 292, 478);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
 		
 		JSlider slider = new JSlider();
-		panel_5.add(slider);
+		slider.setBounds(82, 35, 200, 26);
 		
-		JLabel lblNewLabel_3 = new JLabel("Look R/L     ");
-		panel_5.add(lblNewLabel_3);
+		panel_3.add(slider);
 		
-		JSlider slider_4 = new JSlider();
-		panel_5.add(slider_4);
+		JLabel lblBlink = new JLabel("Blink");
+		lblBlink.setBounds(10, 40, 72, 14);
+		panel_3.add(lblBlink);
 		
-		JLabel lblNewLabel_4 = new JLabel("Furrow Brow");
-		panel_5.add(lblNewLabel_4);
+		JSlider slider_1 = new JSlider();
+		slider_1.setBounds(82, 72, 200, 26);
+		panel_3.add(slider_1);
 		
-		JSlider slider_5 = new JSlider();
-		panel_5.add(slider_5);
-		
-		JLabel lblNewLabel_5 = new JLabel("Raise Brow  ");
-		panel_5.add(lblNewLabel_5);
-		
-		JSlider slider_6 = new JSlider();
-		panel_5.add(slider_6);
-		
-		JLabel lblNewLabel_6 = new JLabel("Smile           ");
-		panel_5.add(lblNewLabel_6);
+		JSlider slider_2 = new JSlider();
+		slider_2.setBounds(82, 109, 200, 26);
+		panel_3.add(slider_2);
 		
 		JSlider slider_3 = new JSlider();
-		panel_5.add(slider_3);
+		slider_3.setBounds(82, 146, 200, 26);
+		panel_3.add(slider_3);
 		
-		JLabel lblNewLabel_7 = new JLabel("Clench         ");
-		panel_5.add(lblNewLabel_7);
+		JSlider slider_4 = new JSlider();
+		slider_4.setBounds(82, 205, 200, 26);
+		panel_3.add(slider_4);
 		
-		JSlider slider_8 = new JSlider();
-		panel_5.add(slider_8);
+		JSlider slider_5 = new JSlider();
+		slider_5.setBounds(82, 242, 200, 26);
+		panel_3.add(slider_5);
 		
-		JLabel lblNewLabel_8 = new JLabel("Left Smirk    ");
-		lblNewLabel_8.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_5.add(lblNewLabel_8);
-		
-		JSlider slider_9 = new JSlider();
-		panel_5.add(slider_9);
-		
-		JLabel lblNewLabel_9 = new JLabel("Right Smirk  ");
-		lblNewLabel_9.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_5.add(lblNewLabel_9);
+		JSlider slider_6 = new JSlider();
+		slider_6.setBounds(82, 293, 200, 26);
+		panel_3.add(slider_6);
 		
 		JSlider slider_7 = new JSlider();
-		panel_5.add(slider_7);
+		slider_7.setBounds(82, 330, 200, 26);
+		panel_3.add(slider_7);
 		
-		JLabel lblNewLabel_10 = new JLabel("Laugh           ");
-		lblNewLabel_10.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_5.add(lblNewLabel_10);
+		JSlider slider_8 = new JSlider();
+		slider_8.setBounds(82, 367, 200, 26);
+		panel_3.add(slider_8);
+		
+		JSlider slider_9 = new JSlider();
+		slider_9.setBounds(82, 404, 200, 26);
+		panel_3.add(slider_9);
 		
 		JSlider slider_10 = new JSlider();
-		panel_5.add(slider_10);
+		slider_10.setBounds(82, 441, 200, 26);
+		panel_3.add(slider_10);
+		
+		JLabel lblRightWink = new JLabel("Right Wink");
+		lblRightWink.setBounds(10, 72, 72, 14);
+		panel_3.add(lblRightWink);
+		
+		JLabel lblLeftWink = new JLabel("Left Wink");
+		lblLeftWink.setBounds(10, 109, 72, 14);
+		panel_3.add(lblLeftWink);
+		
+		JLabel lblLookRl = new JLabel("Look R/L");
+		lblLookRl.setBounds(10, 146, 72, 14);
+		panel_3.add(lblLookRl);
+		
+		JLabel lblFurrowBrow = new JLabel("Furrow Brow");
+		lblFurrowBrow.setBounds(10, 205, 72, 14);
+		panel_3.add(lblFurrowBrow);
+		
+		JLabel lblRaiseBrow = new JLabel("Raise Brow");
+		lblRaiseBrow.setBounds(10, 242, 72, 14);
+		panel_3.add(lblRaiseBrow);
+		
+		JLabel lblSmile = new JLabel("Smile");
+		lblSmile.setBounds(10, 293, 72, 14);
+		panel_3.add(lblSmile);
+		
+		JLabel lblClench = new JLabel("Clench");
+		lblClench.setBounds(10, 330, 72, 14);
+		panel_3.add(lblClench);
+		
+		JLabel lblLeftSmirk = new JLabel("Left Smirk");
+		lblLeftSmirk.setBounds(10, 367, 72, 14);
+		panel_3.add(lblLeftSmirk);
+		
+		JLabel lblRightSmirk = new JLabel("Right Smirk");
+		lblRightSmirk.setBounds(10, 404, 72, 14);
+		panel_3.add(lblRightSmirk);
+		
+		JLabel lblLaugh = new JLabel("Laugh");
+		lblLaugh.setBounds(10, 441, 72, 14);
+		panel_3.add(lblLaugh);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Performance Metrics", null, panel_1, null);
@@ -156,77 +178,77 @@ public class ClientUI extends JFrame {
 		
 		JPanel panel_6 = new JPanel();
 		panel_6.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_6.setBounds(6, 6, 428, 372);
+		panel_6.setBounds(6, 6, 641, 483);
 		panel_1.add(panel_6);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(452, 34, 115, 27);
+		comboBox.setBounds(657, 34, 115, 27);
 		panel_1.add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(607, 34, 108, 27);
+		comboBox_1.setBounds(834, 34, 108, 27);
 		panel_1.add(comboBox_1);
 		
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(459, 100, 108, 27);
+		comboBox_2.setBounds(657, 100, 108, 27);
 		panel_1.add(comboBox_2);
 		
 		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setBounds(607, 100, 108, 27);
+		comboBox_3.setBounds(834, 100, 108, 27);
 		panel_1.add(comboBox_3);
 		
 		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setBounds(459, 163, 108, 27);
+		comboBox_4.setBounds(657, 163, 108, 27);
 		panel_1.add(comboBox_4);
 		
 		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setBounds(607, 163, 108, 27);
+		comboBox_5.setBounds(834, 163, 108, 27);
 		panel_1.add(comboBox_5);
 		
 		JLabel lblInterest = new JLabel("Interest");
-		lblInterest.setBounds(462, 18, 61, 16);
+		lblInterest.setBounds(657, 18, 61, 16);
 		panel_1.add(lblInterest);
 		
 		JLabel lblEngagement = new JLabel("Engagement");
-		lblEngagement.setBounds(613, 18, 77, 16);
+		lblEngagement.setBounds(837, 18, 77, 16);
 		panel_1.add(lblEngagement);
 		
 		JLabel lblStress = new JLabel(" Stress");
-		lblStress.setBounds(462, 84, 61, 16);
+		lblStress.setBounds(666, 84, 61, 16);
 		panel_1.add(lblStress);
 		
 		JLabel lblRelaxation = new JLabel("Relaxation");
-		lblRelaxation.setBounds(617, 84, 77, 16);
+		lblRelaxation.setBounds(837, 84, 77, 16);
 		panel_1.add(lblRelaxation);
 		
 		JLabel lblExcitement = new JLabel("Excitement");
-		lblExcitement.setBounds(469, 148, 74, 16);
+		lblExcitement.setBounds(665, 148, 74, 16);
 		panel_1.add(lblExcitement);
 		
 		JLabel lblFocus = new JLabel("Focus");
-		lblFocus.setBounds(617, 148, 61, 16);
+		lblFocus.setBounds(834, 148, 61, 16);
 		panel_1.add(lblFocus);
 		
 		JLabel lblDisplayLength = new JLabel("Display Length");
-		lblDisplayLength.setBounds(452, 341, 94, 16);
+		lblDisplayLength.setBounds(700, 341, 94, 16);
 		panel_1.add(lblDisplayLength);
 		
 		textField = new JTextField();
-		textField.setBounds(551, 336, 91, 26);
+		textField.setBounds(804, 336, 91, 26);
 		panel_1.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblSeconds = new JLabel("Seconds");
-		lblSeconds.setBounds(654, 341, 61, 16);
+		lblSeconds.setBounds(908, 341, 61, 16);
 		panel_1.add(lblSeconds);
 		
 		JLabel lblTime = new JLabel("Time: ");
-		lblTime.setBounds(506, 11, 39, 16);
+		lblTime.setBounds(731, 11, 39, 16);
 		contentPane.add(lblTime);
 		lblTime.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		JLabel label = new JLabel("");
-		label.setBounds(550, 11, 61, 16);
+		label.setBounds(764, 11, 61, 16);
 		contentPane.add(label);
 	}
 }
