96a6669826d9c78d67f435a1f69be8e6ac69dba8
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-04-05 17:48:38 -0700

    Adding menu for ClientUI

--- project3.team5/src/main/java/project3/client/view/ClientUI.java
+++ project3.team5/src/main/java/project3/client/view/ClientUI.java
@@ -1,13 +1,23 @@
 package project3.client.view;
 
-
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import project3.client.controller.Client;
+import project3.server.view.ServerUI;
+
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 
@@ -15,9 +25,9 @@ public class ClientUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel mainPanel;
-	private JButton connectToServer;
 	private JLabel clockLabel;
 	private JLabel timeLabel;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -30,22 +40,81 @@ public class ClientUI extends JFrame {
 			}
 		});
 	}
-
-
 	
 	public ClientUI() {
+		
 		setTitle("Client");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1015, 616);
+		
 		mainPanel = new JPanel();
 		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		mainPanel.setBackground(Color.WHITE);
 		setContentPane(mainPanel);
 		mainPanel.setLayout(null);
+
+		/**
+		 * Creates Menu with Application and conn
+		 * */
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 698, 22);
+		mainPanel.add(menuBar);
 		
-		connectToServer = new JButton("Connect");
-		connectToServer.setBounds(863, 15, 117, 29);
-		mainPanel.add(connectToServer);
+		JMenu mainMenu = new JMenu("Menu");
+		menuBar.add(mainMenu);
+		
+		JMenu menuApplication = new JMenu("Application");
+		mainMenu.add(menuApplication);
+		
+		JMenu menuConnect = new JMenu("Connect");
+		mainMenu.add(menuConnect);
+		
+		JMenuItem connectServer = new JMenuItem("Server");
+		connectServer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							new ServerUI().setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		menuApplication.add(connectServer);
+		
+		JMenuItem serverConnect = new JMenuItem("Connect to Server");
+		serverConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextField host = new JTextField();
+				JTextField port = new JTextField();
+				
+				Object[] fields= {
+						"Host Name", host,
+						"Port", port
+				};
+				int optionValues = JOptionPane.showConfirmDialog(null, fields, "Connect To Server", JOptionPane.OK_CANCEL_OPTION);
+				switch (optionValues) {
+				case 0:
+					System.out.println("Connect to Server");
+					break;
+				case 1:
+					System.out.println("Cancel");
+					break;
+				}
+			}
+		});
+		menuConnect.add(serverConnect);
+		
+		JMenuItem menuExit = new JMenuItem("Exit");
+		menuExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		mainMenu.add(menuExit);
 		
 		mainPanel.add(new MainTab());
 		clockLabel = new JLabel(new ImageIcon("res/clck.png"));
