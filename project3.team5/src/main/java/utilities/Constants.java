08f33672ea54637756d0bb3143dcdce188f2911a
Harshitha Katpally <32940031+harshithakatpally@users.noreply.github.com>
2018-04-13 18:51:57 -0700

    Added comments in Constants.java

--- project3.team5/src/main/java/utilities/Constants.java
+++ project3.team5/src/main/java/utilities/Constants.java
@@ -1,5 +1,11 @@
+/*
+ * SER516-Project3_Team05
+ */
 package utilities;
 
+/*
+* Contains all the constants used in the application
+*/
 public class Constants {
 	public static final String DEFAULT_IPADDRESS = "localhost";
 	public static final int DEFAULT_PORTNUMBER = 1726;
